68e384969852277851e9af2e2f4ef5a04c0d4da2
Simrri Salazar <ssimrri@gmail.com>
2021-12-01 22:48:26 -0600

    commit tree 1638684e73eadae138b6cbbc501146e85c26a179

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toUpperCase() === name.toUpperCase());
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(driver => driver.startsWith(name))
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name)
+}
